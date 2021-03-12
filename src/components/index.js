25ef32f14c6f94589b0a914431b4e59833596909
FashMuyhee <fashmuyidavid@gmail.com>
2021-03-12 17:42:17 +0100

    add table compoents

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import NavBar from './NavBar';
 import ScrollContainer from './ScrollContainer';
 import Container from './Container';
+import {Table, THead, TBody, TRow, TCell} from './Table';
 
-export {NavBar,ScrollContainer,Container};
+export {NavBar, ScrollContainer, Container, Table, THead, TBody, TRow, TCell};
