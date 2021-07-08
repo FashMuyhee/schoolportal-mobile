e19a487c106e0ab3e7b2f738075d710a04a857ea
FashMuyhee <fashmuyidavid@gmail.com>
2021-07-08 18:55:56 +0100

    imported faq screen

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,9 +1,19 @@
 import Dashboard from './dashboard';
 import Login from './login';
 import Welcome from './welcome';
-import Biodata from './biodata';
+import Biodata from './biodata/index';
 import Result from './result';
 import PaymentHistory from './payment-history';
 import Faq from './faq';
+import ValidatePayment from './validate-payment';
 
-export {Welcome, Login, Dashboard, Biodata, Result, PaymentHistory, Faq};
+export {
+  Welcome,
+  Login,
+  Dashboard,
+  Biodata,
+  Result,
+  PaymentHistory,
+  Faq,
+  ValidatePayment,
+};
