4330025ace0b894bc3dc345690675af66a1cf4d1
FashMuyhee <fashmuyidavid@gmail.com>
2021-07-29 15:27:48 +0100

    rn-firebase

--- src/store/context.js
+++ src/store/context.js
@@ -1,11 +1,25 @@
-import React, {useState, createContext} from 'react';
+import React, {useState, createContext, useEffect} from 'react';
+import auth from '@react-native-firebase/auth';
+
 export const Context = createContext();
 
 export const ContextProvider = (props) => {
-  const [isAuth, setIsAuth] = useState(false);
+  const [isAuth, , setIsAuth] = useState(false);
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState(null);
+
+  const onAuthStateChanged = (user) => {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  };
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
 
   return (
-    <Context.Provider value={{isAuth, setIsAuth}}>
+    <Context.Provider value={{isAuth, setIsAuth, user, initializing}}>
       {props.children}
     </Context.Provider>
   );
